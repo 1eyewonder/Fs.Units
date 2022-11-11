c7b14accbcbafac2af514070d261db4563c92f99
Bronson Bata <bronsonbata@gmail.com>
2022-11-11 17:14:54 -0600

    Started adding C# interface for library

---
+++ Fs.Units.CSharp/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Fs.Units.Length;
+global using Fs.Units.Time;
+global using Fs.Units.Mass;
+global using Fs.Units.Temperature;
---
+++ Fs.Units.CSharp/Simple/Time.cs
@@ -0,0 +1,272 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Fs.Units.CSharp.Time;
+
+public static class NanosecondExtensions
+{
+    public static double NanosecondToMicroseconds(this double x) => Nanosecond.toMicroSeconds(x);
+    public static double NanosecondToMilliseconds(this double x) => Nanosecond.toMilliSeconds(x);
+    public static double NanosecondToSeconds(this double x) => Nanosecond.toSeconds(x);
+    public static double NanosecondToMinutes(this double x) => Nanosecond.toMinutes(x);
+    public static double NanosecondToHours(this double x) => Nanosecond.toHours(x);
+    public static double NanosecondToDays(this double x) => Nanosecond.toDays(x);
+    public static double NanosecondToWeeks(this double x) => Nanosecond.toWeeks(x);
+    public static double NanosecondToFortnights(this double x) => Nanosecond.toFortnights(x);
+    public static double NanosecondToMonths(this double x) => Nanosecond.toMonths(x);
+    public static double NanosecondToYears(this double x) => Nanosecond.toYears(x);
+    public static double NanosecondToDecades(this double x) => Nanosecond.toDecades(x);
+    public static double NanosecondToCenturies(this double x) => Nanosecond.toCenturies(x);
+    public static double NanosecondToMillennia(this double x) => Nanosecond.toMillennia(x);
+    public static double NanosecondToEons(this double x) => Nanosecond.toEons(x);
+}
+
+public static class MicrosecondExtensions
+{
+    public static double MicrosecondToNanoseconds(this double x) => Microsecond.toNanoSeconds(x);
+    public static double MicrosecondToMilliseconds(this double x) => Microsecond.toMilliSeconds(x);
+    public static double MicrosecondToSeconds(this double x) => Microsecond.toSeconds(x);
+    public static double MicrosecondToMinutes(this double x) => Microsecond.toMinutes(x);
+    public static double MicrosecondToHours(this double x) => Microsecond.toHours(x);
+    public static double MicrosecondToDays(this double x) => Microsecond.toDays(x);
+    public static double MicrosecondToWeeks(this double x) => Microsecond.toWeeks(x);
+    public static double MicrosecondToFortnights(this double x) => Microsecond.toFortnights(x);
+    public static double MicrosecondToMonths(this double x) => Microsecond.toMonths(x);
+    public static double MicrosecondToYears(this double x) => Microsecond.toYears(x);
+    public static double MicrosecondToDecades(this double x) => Microsecond.toDecades(x);
+    public static double MicrosecondToCenturies(this double x) => Microsecond.toCenturies(x);
+    public static double MicrosecondToMillennia(this double x) => Microsecond.toMillennia(x);
+    public static double MicrosecondToEons(this double x) => Microsecond.toEons(x);
+}
+
+public static class MillisecondExtensions
+{
+    public static double MillisecondToNanoseconds(this double x) => Millisecond.toNanoSeconds(x);
+    public static double MillisecondToMicroseconds(this double x) => Millisecond.toMicroSeconds(x);
+    public static double MillisecondToSeconds(this double x) => Millisecond.toSeconds(x);
+    public static double MillisecondToMinutes(this double x) => Millisecond.toMinutes(x);
+    public static double MillisecondToHours(this double x) => Millisecond.toHours(x);
+    public static double MillisecondToDays(this double x) => Millisecond.toDays(x);
+    public static double MillisecondToWeeks(this double x) => Millisecond.toWeeks(x);
+    public static double MillisecondToFortnights(this double x) => Millisecond.toFortnights(x);
+    public static double MillisecondToMonths(this double x) => Millisecond.toMonths(x);
+    public static double MillisecondToYears(this double x) => Millisecond.toYears(x);
+    public static double MillisecondToDecades(this double x) => Millisecond.toDecades(x);
+    public static double MillisecondToCenturies(this double x) => Millisecond.toCenturies(x);
+    public static double MillisecondToMillennia(this double x) => Millisecond.toMillennia(x);
+    public static double MillisecondToEons(this double x) => Millisecond.toEons(x);
+}
+
+public static class SecondExtensions
+{
+    public static double SecondToNanoseconds(this double x) => Second.toNanoSeconds(x);
+    public static double SecondToMicroseconds(this double x) => Second.toMicroSeconds(x);
+    public static double SecondToMilliseconds(this double x) => Second.toMilliSeconds(x);
+    public static double SecondToMinutes(this double x) => Second.toMinutes(x);
+    public static double SecondToHours(this double x) => Second.toHours(x);
+    public static double SecondToDays(this double x) => Second.toDays(x);
+    public static double SecondToWeeks(this double x) => Second.toWeeks(x);
+    public static double SecondToFortnights(this double x) => Second.toFortnights(x);
+    public static double SecondToMonths(this double x) => Second.toMonths(x);
+    public static double SecondToYears(this double x) => Second.toYears(x);
+    public static double SecondToDecades(this double x) => Second.toDecades(x);
+    public static double SecondToCenturies(this double x) => Second.toCenturies(x);
+    public static double SecondToMillennia(this double x) => Second.toMillennia(x);
+    public static double SecondToEons(this double x) => Second.toEons(x);
+}
+
+public static class MinuteExtensions
+{
+    public static double MinuteToNanoseconds(this double x) => Minute.toNanoSeconds(x);
+    public static double MinuteToMicroseconds(this double x) => Minute.toMicroSeconds(x);
+    public static double MinuteToMilliseconds(this double x) => Minute.toMilliSeconds(x);
+    public static double MinuteToSeconds(this double x) => Minute.toSeconds(x);
+    public static double MinuteToHours(this double x) => Minute.toHours(x);
+    public static double MinuteToDays(this double x) => Minute.toDays(x);
+    public static double MinuteToWeeks(this double x) => Minute.toWeeks(x);
+    public static double MinuteToFortnights(this double x) => Minute.toFortnights(x);
+    public static double MinuteToMonths(this double x) => Minute.toMonths(x);
+    public static double MinuteToYears(this double x) => Minute.toYears(x);
+    public static double MinuteToDecades(this double x) => Minute.toDecades(x);
+    public static double MinuteToCenturies(this double x) => Minute.toCenturies(x);
+    public static double MinuteToMillennia(this double x) => Minute.toMillennia(x);
+    public static double MinuteToEons(this double x) => Minute.toEons(x);
+}
+
+public static class HourExtensions
+{
+    public static double HourToNanoseconds(this double x) => Hour.toNanoSeconds(x);
+    public static double HourToMicroseconds(this double x) => Hour.toMicroSeconds(x);
+    public static double HourToMilliseconds(this double x) => Hour.toMilliSeconds(x);
+    public static double HourToSeconds(this double x) => Hour.toSeconds(x);
+    public static double HourToMinutes(this double x) => Hour.toMinutes(x);
+    public static double HourToDays(this double x) => Hour.toDays(x);
+    public static double HourToWeeks(this double x) => Hour.toWeeks(x);
+    public static double HourToFortnights(this double x) => Hour.toFortnights(x);
+    public static double HourToMonths(this double x) => Hour.toMonths(x);
+    public static double HourToYears(this double x) => Hour.toYears(x);
+    public static double HourToDecades(this double x) => Hour.toDecades(x);
+    public static double HourToCenturies(this double x) => Hour.toCenturies(x);
+    public static double HourToMillennia(this double x) => Hour.toMillennia(x);
+    public static double HourToEons(this double x) => Hour.toEons(x);
+}
+
+public static class DayExtensions
+{
+    public static double DayToNanoseconds(this double x) => Day.toNanoSeconds(x);
+    public static double DayToMicroseconds(this double x) => Day.toMicroSeconds(x);
+    public static double DayToMilliseconds(this double x) => Day.toMilliSeconds(x);
+    public static double DayToSeconds(this double x) => Day.toSeconds(x);
+    public static double DayToMinutes(this double x) => Day.toMinutes(x);
+    public static double DayToHours(this double x) => Day.toHours(x);
+    public static double DayToWeeks(this double x) => Day.toWeeks(x);
+    public static double DayToFortnights(this double x) => Day.toFortnights(x);
+    public static double DayToMonths(this double x) => Day.toMonths(x);
+    public static double DayToYears(this double x) => Day.toYears(x);
+    public static double DayToDecades(this double x) => Day.toDecades(x);
+    public static double DayToCenturies(this double x) => Day.toCenturies(x);
+    public static double DayToMillennia(this double x) => Day.toMillennia(x);
+    public static double DayToEons(this double x) => Day.toEons(x);
+}
+
+public static class WeekExtensions
+{
+    public static double WeekToNanoseconds(this double x) => Week.toNanoSeconds(x);
+    public static double WeekToMicroseconds(this double x) => Week.toMicroSeconds(x);
+    public static double WeekToMilliseconds(this double x) => Week.toMilliSeconds(x);
+    public static double WeekToSeconds(this double x) => Week.toSeconds(x);
+    public static double WeekToMinutes(this double x) => Week.toMinutes(x);
+    public static double WeekToHours(this double x) => Week.toHours(x);
+    public static double WeekToDays(this double x) => Week.toDays(x);
+    public static double WeekToFortnights(this double x) => Week.toFortnights(x);
+    public static double WeekToMonths(this double x) => Week.toMonths(x);
+    public static double WeekToYears(this double x) => Week.toYears(x);
+    public static double WeekToDecades(this double x) => Week.toDecades(x);
+    public static double WeekToCenturies(this double x) => Week.toCenturies(x);
+    public static double WeekToMillennia(this double x) => Week.toMillennia(x);
+    public static double WeekToEons(this double x) => Week.toEons(x);
+}
+
+public static class FortnightExtensions
+{
+    public static double FortnightToNanoseconds(this double x) => Fortnight.toNanoSeconds(x);
+    public static double FortnightToMicroseconds(this double x) => Fortnight.toMicroSeconds(x);
+    public static double FortnightToMilliseconds(this double x) => Fortnight.toMilliSeconds(x);
+    public static double FortnightToSeconds(this double x) => Fortnight.toSeconds(x);
+    public static double FortnightToMinutes(this double x) => Fortnight.toMinutes(x);
+    public static double FortnightToHours(this double x) => Fortnight.toHours(x);
+    public static double FortnightToDays(this double x) => Fortnight.toDays(x);
+    public static double FortnightToWeeks(this double x) => Fortnight.toWeeks(x);
+    public static double FortnightToMonths(this double x) => Fortnight.toMonths(x);
+    public static double FortnightToYears(this double x) => Fortnight.toYears(x);
+    public static double FortnightToDecades(this double x) => Fortnight.toDecades(x);
+    public static double FortnightToCenturies(this double x) => Fortnight.toCenturies(x);
+    public static double FortnightToMillennia(this double x) => Fortnight.toMillennia(x);
+    public static double FortnightToEons(this double x) => Fortnight.toEons(x);
+}
+
+public static class MonthExtensions
+{
+    public static double MonthToNanoseconds(this double x) => Month.toNanoSeconds(x);
+    public static double MonthToMicroseconds(this double x) => Month.toMicroSeconds(x);
+    public static double MonthToMilliseconds(this double x) => Month.toMilliSeconds(x);
+    public static double MonthToSeconds(this double x) => Month.toSeconds(x);
+    public static double MonthToMinutes(this double x) => Month.toMinutes(x);
+    public static double MonthToHours(this double x) => Month.toHours(x);
+    public static double MonthToDays(this double x) => Month.toDays(x);
+    public static double MonthToWeeks(this double x) => Month.toWeeks(x);
+    public static double MonthToFortnights(this double x) => Month.toFortnights(x);
+    public static double MonthToYears(this double x) => Month.toYears(x);
+    public static double MonthToDecades(this double x) => Month.toDecades(x);
+    public static double MonthToCenturies(this double x) => Month.toCenturies(x);
+    public static double MonthToMillennia(this double x) => Month.toMillennia(x);
+    public static double MonthToEons(this double x) => Month.toEons(x);
+}
+
+public static class YearExtensions
+{
+    public static double YearToNanoseconds(this double x) => Year.toNanoSeconds(x);
+    public static double YearToMicroseconds(this double x) => Year.toMicroSeconds(x);
+    public static double YearToMilliseconds(this double x) => Year.toMilliSeconds(x);
+    public static double YearToSeconds(this double x) => Year.toSeconds(x);
+    public static double YearToMinutes(this double x) => Year.toMinutes(x);
+    public static double YearToHours(this double x) => Year.toHours(x);
+    public static double YearToDays(this double x) => Year.toDays(x);
+    public static double YearToWeeks(this double x) => Year.toWeeks(x);
+    public static double YearToFortnights(this double x) => Year.toFortnights(x);
+    public static double YearToMonths(this double x) => Year.toMonths(x);
+    public static double YearToDecades(this double x) => Year.toDecades(x);
+    public static double YearToCenturies(this double x) => Year.toCenturies(x);
+    public static double YearToMillennia(this double x) => Year.toMillennia(x);
+    public static double YearToEons(this double x) => Year.toEons(x);
+}
+
+public static class DecadeExtensions
+{
+    public static double DecadeToNanoseconds(this double x) => Decade.toNanoSeconds(x);
+    public static double DecadeToMicroseconds(this double x) => Decade.toMicroSeconds(x);
+    public static double DecadeToMilliseconds(this double x) => Decade.toMilliSeconds(x);
+    public static double DecadeToSeconds(this double x) => Decade.toSeconds(x);
+    public static double DecadeToMinutes(this double x) => Decade.toMinutes(x);
+    public static double DecadeToHours(this double x) => Decade.toHours(x);
+    public static double DecadeToDays(this double x) => Decade.toDays(x);
+    public static double DecadeToWeeks(this double x) => Decade.toWeeks(x);
+    public static double DecadeToFortnights(this double x) => Decade.toFortnights(x);
+    public static double DecadeToMonths(this double x) => Decade.toMonths(x);
+    public static double DecadeToYears(this double x) => Decade.toYears(x);
+    public static double DecadeToCenturies(this double x) => Decade.toCenturies(x);
+    public static double DecadeToMillennia(this double x) => Decade.toMillennia(x);
+    public static double DecadeToEons(this double x) => Decade.toEons(x);
+}
+
+public static class CenturyExtensions
+{
+    public static double CenturyToNanoseconds(this double x) => Century.toNanoSeconds(x);
+    public static double CenturyToMicroseconds(this double x) => Century.toMicroSeconds(x);
+    public static double CenturyToMilliseconds(this double x) => Century.toMilliSeconds(x);
+    public static double CenturyToSeconds(this double x) => Century.toSeconds(x);
+    public static double CenturyToMinutes(this double x) => Century.toMinutes(x);
+    public static double CenturyToHours(this double x) => Century.toHours(x);
+    public static double CenturyToDays(this double x) => Century.toDays(x);
+    public static double CenturyToWeeks(this double x) => Century.toWeeks(x);
+    public static double CenturyToFortnights(this double x) => Century.toFortnights(x);
+    public static double CenturyToMonths(this double x) => Century.toMonths(x);
+    public static double CenturyToYears(this double x) => Century.toYears(x);
+    public static double CenturyToDecades(this double x) => Century.toDecades(x);
+    public static double CenturyToMillennia(this double x) => Century.toMillennia(x);
+    public static double CenturyToEons(this double x) => Century.toEons(x);
+}
+
+public static class MillenniumExtensions
+{
+    public static double MillenniumToNanoseconds(this double x) => Millennium.toNanoSeconds(x);
+    public static double MillenniumToMicroseconds(this double x) => Millennium.toMicroSeconds(x);
+    public static double MillenniumToMilliseconds(this double x) => Millennium.toMilliSeconds(x);
+    public static double MillenniumToSeconds(this double x) => Millennium.toSeconds(x);
+    public static double MillenniumToMinutes(this double x) => Millennium.toMinutes(x);
+    public static double MillenniumToHours(this double x) => Millennium.toHours(x);
+    public static double MillenniumToDays(this double x) => Millennium.toDays(x);
+    public static double MillenniumToWeeks(this double x) => Millennium.toWeeks(x);
+    public static double MillenniumToFortnights(this double x) => Millennium.toFortnights(x);
+    public static double MillenniumToMonths(this double x) => Millennium.toMonths(x);
+    public static double MillenniumToYears(this double x) => Millennium.toYears(x);
+    public static double MillenniumToDecades(this double x) => Millennium.toDecades(x);
+    public static double MillenniumToCenturies(this double x) => Millennium.toCenturies(x);
+    public static double MillenniumToEons(this double x) => Millennium.toEons(x);
+}
+
+public static class EonExtensions
+{
+    public static double EonToNanoseconds(this double x) => Eon.toNanoSeconds(x);
+    public static double EonToMicroseconds(this double x) => Eon.toMicroSeconds(x);
+    public static double EonToMilliseconds(this double x) => Eon.toMilliSeconds(x);
+    public static double EonToSeconds(this double x) => Eon.toSeconds(x);
+    public static double EonToMinutes(this double x) => Eon.toMinutes(x);
+    public static double EonToHours(this double x) => Eon.toHours(x);
+    public static double EonToDays(this double x) => Eon.toDays(x);
+    public static double EonToWeeks(this double x) => Eon.toWeeks(x);
+    public static double EonToFortnights(this double x) => Eon.toFortnights(x);
+    public static double EonToMonths(this double x) => Eon.toMonths(x);
+    public static double EonToYears(this double x) => Eon.toYears(x);
+    public static double EonToDecades(this double x) => Eon.toDecades(x);
+    public static double EonToCenturies(this double x) => Eon.toCenturies(x);
+    public static double EonToMillennia(this double x) => Eon.toMillennia(x);
+}
